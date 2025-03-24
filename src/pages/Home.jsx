8b17683a82f5c4fb71ebca2d91ba17c589ac5578
Charitha-Prabashwara <prabhashwara.seu@gmail.com>
2025-03-24 14:52:41 +0530

    homepage

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Head from "../components/homepage/Head";
+function Home(){
+    return(
+      <>
+      <Head />
+      <h1 class="text-3xl font-bold underline">
+             Hello world!
+     </h1>
+      </> 
+    )
+}
+
+export default Home;
